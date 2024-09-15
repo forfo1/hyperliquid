a25cb0476a7f6c44bb59f742f4dc7d220d1c000b
forfo1 <166167559+forfo1@users.noreply.github.com>
2024-09-15 10:57:28 +0900

    Update Hyperliquid-nonvalidator.sh

--- Hyperliquid-nonvalidator.sh
+++ Hyperliquid-nonvalidator.sh
@@ -23,7 +23,7 @@ read_color() {
     read "$@"
 }
 
-# Function to prompt user to press any key to continue
+# Function press any key to continue
 press_any_key() {
     echo -e "\e[36mPress any key to continue...\e[0m"
     read -n 1 -s  # -n 1 reads 1 character, -s hides the input
@@ -43,7 +43,7 @@ sudo update-locale LANG=ko_KR.UTF-8 LC_MESSAGES=POSIX
 print_color "36" "한글설치완료"
 
 
-# commands
+# 
 print_color "32" "sudo apt update && sudo apt upgrade -y"
 sudo apt update && sudo apt upgrade -y
 print_color "32" "sudo apt -qy install curl git jq lz4 build-essential screen"
@@ -51,21 +51,6 @@ sudo apt -qy install curl git jq lz4 build-essential screen
 print_color "32" "sudo apt autoremove -y"
 sudo apt autoremove -y
 
-
-print_color "32" "curl -fsSL https://get.docker.com -o get-docker.sh "
-curl -fsSL https://get.docker.com -o get-docker.sh 
-print_color "32" "sudo sh get-docker.sh"
-sudo sh get-docker.sh
-print_color "36" "docker version"
-docker version
-
-
-print_color "32" "sudo curl -L https://github.com/docker/compose/releases/download/$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose"
-sudo curl -L https://github.com/docker/compose/releases/download/$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)/docker-compose-$(uname -s)-$(uname -m) -o /usr/bin/docker-compose
-print_color "32" "sudo chmod 755 /usr/bin/docker-compose"
-sudo chmod 755 /usr/bin/docker-compose
-print_color "36" "docker-compose version"
-docker-compose version
 
 
 # Set username
@@ -81,7 +66,7 @@ useradd -m -s /bin/bash $USERNAME
 # Set password
 echo "$USERNAME:$PASSWORD" | chpasswd
 
-# Add user to sudo group
+
 sudo usermod -aG sudo $USERNAME
 sudo usermod -aG docker $USERNAME
 echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
@@ -90,13 +75,13 @@ print_color "32" "hluser 생성, sudo 권한 부여."
 
 
 
-print_color "32" "Hyperliquid node 실행"
+print_color "32" "Hyperliquid node 설치"
 sudo -u $USERNAME bash << EOF
 cd /home/$USERNAME
 echo '{"chain": "Testnet"}' > ~/visor.json
 curl https://binaries.hyperliquid.xyz/Testnet/hl-visor > ~/hl-visor && chmod a+x ~/hl-visor
 EOF
-print_color "31" "이제 hluser에서 ~/hl-visor run-non-validator커맨드로 노드를 실행할 수 있습니다." 
+print_color "31" "이제 hluser에서 ~/hl-visor run-non-validator 커맨드로 노드를 실행할 수 있습니다." 
 
 
 
